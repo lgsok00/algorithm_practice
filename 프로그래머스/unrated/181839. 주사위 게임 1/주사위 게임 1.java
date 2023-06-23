fcb868afe9bd97d2d4b78d04a17b8709e84aca4f
lgsok00 <80325051+lgsok00@users.noreply.github.com>
2023-06-24 00:17:56 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
         int answer = 0;
-        if (a % 2 == 1 && b % 2 == 1) {
+        if (a % 2 != 0 && b % 2 != 0) {
             answer = a * a + b * b;
-        } else if (a % 2 == 1 && b % 2 == 0) {
-            answer = 2 * (a + b);
-        } else if (a % 2 == 0 && b % 2 == 1) {
+        } else if (a % 2 != 0 || b % 2 != 0) {
             answer = 2 * (a + b);
         } else {
             answer = Math.abs(a - b);
