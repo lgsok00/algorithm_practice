404263fb43d36e45d7c05fee54826ecd32b0fe8e
lgsok00 <80325051+lgsok00@users.noreply.github.com>
2023-06-25 16:55:45 +0900

    [unrated] Title: 5명씩, Time: 0.09 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+class Solution {
+    public String[] solution(String[] names) {
+        List<String> answerList = new ArrayList<>();
+        
+        while (names.length > 0) {
+            String[] group = Arrays.copyOfRange(names, 0, Math.min(5, names.length));
+            answerList.add(group[0]);
+            names = Arrays.copyOfRange(names, Math.min(5, names.length), names.length);
+        }
+        
+        String[] answer = new String[answerList.size()];
+        answer = answerList.toArray(answer);
+        return answer;
+    }
+}
