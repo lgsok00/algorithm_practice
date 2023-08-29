2ea26136ae25f6528c9841e0652d9777dd94482c
lgsok00 <80325051+lgsok00@users.noreply.github.com>
2023-08-29 09:31:15 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0, 0};
+        for (int i = 0; i < keyinput.length; i++) {
+            switch (keyinput[i]) {
+                case "right":
+                    if (answer[0] >= board[0] / 2) {
+                        continue;
+                    }
+                    answer[0]++;
+                    break;
+                case "left":
+                    if (answer[0] <= board[0] / 2 * -1) {
+                        continue;
+                    }
+                    answer[0]--;
+                    break;
+                case "up":
+                    if (answer[1] >= board[1] / 2) {
+                        continue;
+                    }
+                    answer[1]++;
+                    break;
+                case "down":
+                    if (answer[1] <= board[1] / 2 * -1) {
+                        continue;
+                    }
+                    answer[1]--;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
