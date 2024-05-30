fe0c266e17a4e1d54ebe18a02850163aa1f4029b
jjinn_2 <80325051+lgsok00@users.noreply.github.com>
2024-05-30 15:24:33 +0900

    [Bronze II] Title: 단어의 개수, Time: 248 ms, Memory: 20012 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,16 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/*
+  1. BufferedReader 로 입력 받기
+  2. StringTokenizer 로 공백(" ")을 기준으로 문자열 분리하기
+ */
 public class Main {
-    
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        
-        String s = in.nextLine();
-        in.close();
-        
-        StringTokenizer st = new StringTokenizer(s, " ");
-        
-        System.out.println(st.countTokens());
-    }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    System.out.println(st.countTokens());
+  }
 }
