c72746041f5e575614887a7b25929a8846b654e5
jjinn_2 <80325051+lgsok00@users.noreply.github.com>
2024-06-03 15:24:40 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 232 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,37 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        int n = in.nextInt();
-        int m = in.nextInt();
-        
-        int[][] arr1 = new int[n][m];
-        int[][] arr2 = new int[n][m];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr1[i][j] = in.nextInt();
-            }
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr2[i][j] = in.nextInt();
-            }
-        }
-        
-        in.close();
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(arr1[i][j] + arr2[i][j] + " ");
-                if (j == m - 1) {
-                    System.out.println();
-                }
-            }
-        }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[][] arr = new int[n][m];
+
+    // 첫번째 행렬에 값 추가
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
+
+    // 두 번째 행렬에 값을 넣어주면서 StringBuilder 애 값 저장
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < m; j++) {
+        sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
 }
