90416b98100fa5cd7a0e7cf5f6aa60291f12e3f2
jjinn_2 <80325051+lgsok00@users.noreply.github.com>
2024-06-14 11:22:01 +0900

    [Gold IV] Title: 최단경로, Time: 892 ms, Memory: 132936 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,94 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class Main {
+
+  static List<Node>[] nodes;
+  static boolean[] visited;
+  static int[] distance;
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String[] ve = br.readLine().split(" ");
+    int v = Integer.parseInt(ve[0]);
+    int e = Integer.parseInt(ve[1]);
+
+    nodes = new ArrayList[v + 1];
+    visited = new boolean[v + 1];
+    distance = new int[v + 1];
+
+    int start = Integer.parseInt(br.readLine());
+    for (int i = 0; i <= v; i++) {
+      nodes[i] = new ArrayList<>();
+      distance[i] = Integer.MAX_VALUE;
+    }
+
+    for (int i = 0; i < e; i++) {
+      String[] edge = br.readLine().split(" ");
+
+      int from = Integer.parseInt(edge[0]);
+      int to = Integer.parseInt(edge[1]);
+      int weight = Integer.parseInt(edge[2]);
+
+      nodes[from].add(new Node(to, weight));
+    }
+
+    dijkstra(start);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= v; i++) {
+      if (distance[i] == Integer.MAX_VALUE) {
+        sb.append("INF").append("\n");
+      } else {
+        sb.append(distance[i]).append("\n");
+      }
+    }
+    System.out.println(sb);
+  }
+
+  private static void dijkstra(int start) {
+    PriorityQueue<Node> pq = new PriorityQueue<>();
+    distance[start] = 0;
+
+    pq.offer(new Node(start, 0));
+
+    while (!pq.isEmpty()) {
+      // 최소 가중치를 가진 노드
+      Node cur = pq.poll();
+      visited[cur.to] = true;
+
+      for (Node node : nodes[cur.to]) {
+        if (visited[node.to]) {
+          continue;
+        }
+
+        if (distance[node.to] > distance[cur.to] + node.weight) {
+          distance[node.to] = distance[cur.to] + node.weight;
+          pq.offer(new Node(node.to, distance[node.to]));
+        }
+      }
+    }
+  }
+}
+
+class Node implements Comparable<Node> {
+
+  public int to;
+  public int weight;
+
+  @Override
+  public int compareTo(Node o) {
+    return this.weight - o.weight;
+  }
+
+  public Node(int end, int cost) {
+    this.to = end;
+    this.weight = cost;
+  }
+}
